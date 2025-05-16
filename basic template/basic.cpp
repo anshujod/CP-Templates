f83ee46f2a467b7e35b74ffc0e4687c9574c1e7e
Anshu Prakash <anshuprakash55@gmail.com>
2025-05-16 10:49:33 +0530

    Added new folder with C++ template/code

---
+++ basic template/basic.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Fast I/O
+#define fast_io ios::sync_with_stdio(false); cin.tie(NULL);
+
+// Type aliases
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+
+// Useful constants
+const int INF = 1e9;
+const ll MOD = 1e9 + 7;
+
+// For multiple test cases
+void solve() {
+    // Write code for one test case here
+}
+
+int main() {
+    fast_io
+
+    int t = 1;
+    cin >> t;          // Comment this line if only one test case
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
